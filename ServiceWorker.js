e6324d1b2e5867c83d718333d6474f733c79fe36
Dmitriy Merzlyakov <48850050+Collider24@users.noreply.github.com>
2025-03-30 21:41:25 +0700

    test webview

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "ARGUS-ARGUS-Web-Viewer-0.1.0";
+const contentToCache = [
+    "Build/ARGUS-Web-Viewer.loader.js",
+    "Build/ARGUS-Web-Viewer.framework.js.br",
+    "Build/ARGUS-Web-Viewer.data.br",
+    "Build/ARGUS-Web-Viewer.wasm.br",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
